6faf0d0eb92b3d960bc6ae0ffb99475c6dc83084
Markus Mittendrein <git@maxmitti.at>
2026-06-14 18:18:26 +0200

    clang-format macnowplaying.mm

--- mac/macnowplaying.mm
+++ mac/macnowplaying.mm
@@ -23,150 +23,149 @@
 
 #include "macnowplaying.h"
 
-#include "mpd-interface/mpdconnection.h"
 #include "gui/currentcover.h"
 #include "gui/stdactions.h"
+#include "mpd-interface/mpdconnection.h"
 
-#include <QPointer>
 #import <AppKit/AppKit.h>
 #import <MediaPlayer/MediaPlayer.h>
+#include <QPointer>
 
-struct MacNowPlaying::impl
-{
-    QPointer<MPDStatus> status;
-    QString currentCover;
-    Song currentSong;
-    NSMutableDictionary *nowPlayingInfo;
+struct MacNowPlaying::impl {
+	QPointer<MPDStatus> status;
+	QString currentCover;
+	Song currentSong;
+	NSMutableDictionary* nowPlayingInfo;
 
-    impl()
-        : nowPlayingInfo([[NSMutableDictionary alloc] init])
-    {}
+	impl()
+		: nowPlayingInfo([[NSMutableDictionary alloc] init])
+	{}
 };
 
-MacNowPlaying::MacNowPlaying(QObject *p)
-    : QObject(p)
-    , pimpl(new impl())
+MacNowPlaying::MacNowPlaying(QObject* p)
+	: QObject(p), pimpl(new impl())
 {
 	connect(this, &MacNowPlaying::setSeekId, MPDConnection::self(), &MPDConnection::setSeekId);
 	connect(CurrentCover::self(), &CurrentCover::coverFile, this, &MacNowPlaying::updateCurrentCover);
 
-	MPRemoteCommandCenter *commandCenter = [MPRemoteCommandCenter sharedCommandCenter];
+	MPRemoteCommandCenter* commandCenter = [MPRemoteCommandCenter sharedCommandCenter];
 
-    [commandCenter.pauseCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull) {
-        if (!pimpl->status.isNull() && MPDState_Playing==pimpl->status->state()) {
-            StdActions::self()->playPauseTrackAction->trigger();
-        }
-        return MPRemoteCommandHandlerStatusSuccess;
-    }];
-    [commandCenter.playCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull) {
-        if (!pimpl->status.isNull() && pimpl->status->playlistLength()>0 && MPDState_Playing!=pimpl->status->state()) {
-            StdActions::self()->playPauseTrackAction->trigger();
-        }
-        return MPRemoteCommandHandlerStatusSuccess;
-    }];
-    [commandCenter.stopCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull) {
-        StdActions::self()->stopPlaybackAction->trigger();
-        return MPRemoteCommandHandlerStatusSuccess;
-    }];
-    [commandCenter.togglePlayPauseCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull) {
-        StdActions::self()->playPauseTrackAction->trigger();
-        return MPRemoteCommandHandlerStatusSuccess;
-    }];
-    [commandCenter.nextTrackCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull) {
-        StdActions::self()->nextTrackAction->trigger();
-        return MPRemoteCommandHandlerStatusSuccess;
-    }];
-    [commandCenter.previousTrackCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull) {
-        StdActions::self()->prevTrackAction->trigger();
-        return MPRemoteCommandHandlerStatusSuccess;
-    }];
-    [commandCenter.changePlaybackPositionCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent * _Nonnull event) {
-        MPChangePlaybackPositionCommandEvent* seekEvent = static_cast<MPChangePlaybackPositionCommandEvent *>(event);
-        emit setSeekId(-1, seekEvent.positionTime);
-        return MPRemoteCommandHandlerStatusSuccess;
-    }];
+	[commandCenter.pauseCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent* _Nonnull) {
+	  if (!pimpl->status.isNull() && MPDState_Playing == pimpl->status->state()) {
+		  StdActions::self()->playPauseTrackAction->trigger();
+	  }
+	  return MPRemoteCommandHandlerStatusSuccess;
+	}];
+	[commandCenter.playCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent* _Nonnull) {
+	  if (!pimpl->status.isNull() && pimpl->status->playlistLength() > 0 && MPDState_Playing != pimpl->status->state()) {
+		  StdActions::self()->playPauseTrackAction->trigger();
+	  }
+	  return MPRemoteCommandHandlerStatusSuccess;
+	}];
+	[commandCenter.stopCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent* _Nonnull) {
+	  StdActions::self()->stopPlaybackAction->trigger();
+	  return MPRemoteCommandHandlerStatusSuccess;
+	}];
+	[commandCenter.togglePlayPauseCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent* _Nonnull) {
+	  StdActions::self()->playPauseTrackAction->trigger();
+	  return MPRemoteCommandHandlerStatusSuccess;
+	}];
+	[commandCenter.nextTrackCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent* _Nonnull) {
+	  StdActions::self()->nextTrackAction->trigger();
+	  return MPRemoteCommandHandlerStatusSuccess;
+	}];
+	[commandCenter.previousTrackCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent* _Nonnull) {
+	  StdActions::self()->prevTrackAction->trigger();
+	  return MPRemoteCommandHandlerStatusSuccess;
+	}];
+	[commandCenter.changePlaybackPositionCommand addTargetWithHandler:^MPRemoteCommandHandlerStatus(MPRemoteCommandEvent* _Nonnull event) {
+	  MPChangePlaybackPositionCommandEvent* seekEvent = static_cast<MPChangePlaybackPositionCommandEvent*>(event);
+	  emit setSeekId(-1, seekEvent.positionTime);
+	  return MPRemoteCommandHandlerStatusSuccess;
+	}];
 
-    commandCenter.changeRepeatModeCommand.enabled = NO;
-    commandCenter.changeShuffleModeCommand.enabled = NO;
-    commandCenter.changePlaybackRateCommand.enabled = NO;
-    commandCenter.seekBackwardCommand.enabled = NO;
-    commandCenter.seekForwardCommand.enabled = NO;
-    commandCenter.skipBackwardCommand.enabled = NO;
-    commandCenter.skipForwardCommand.enabled = NO;
-    commandCenter.ratingCommand.enabled = NO;
-    commandCenter.likeCommand.enabled = NO;
-    commandCenter.dislikeCommand.enabled = NO;
-    commandCenter.bookmarkCommand.enabled = NO;
-    commandCenter.enableLanguageOptionCommand.enabled = NO;
-    commandCenter.disableLanguageOptionCommand.enabled = NO;
+	commandCenter.changeRepeatModeCommand.enabled = NO;
+	commandCenter.changeShuffleModeCommand.enabled = NO;
+	commandCenter.changePlaybackRateCommand.enabled = NO;
+	commandCenter.seekBackwardCommand.enabled = NO;
+	commandCenter.seekForwardCommand.enabled = NO;
+	commandCenter.skipBackwardCommand.enabled = NO;
+	commandCenter.skipForwardCommand.enabled = NO;
+	commandCenter.ratingCommand.enabled = NO;
+	commandCenter.likeCommand.enabled = NO;
+	commandCenter.dislikeCommand.enabled = NO;
+	commandCenter.bookmarkCommand.enabled = NO;
+	commandCenter.enableLanguageOptionCommand.enabled = NO;
+	commandCenter.disableLanguageOptionCommand.enabled = NO;
 }
 
 MacNowPlaying::~MacNowPlaying()
 {}
 
-void MacNowPlaying::updateStatus(MPDStatus * const status)
+void MacNowPlaying::updateStatus(MPDStatus* const status)
 {
-    // If the current song has not yet been updated, reject this status
-    // update and wait for the next unless the play queue has recently
-    // been emptied or the connection to MPD has been lost ...
-    if (status->songId()!=pimpl->currentSong.id && status->songId()!=-1) {
-        return;
-    }
+	// If the current song has not yet been updated, reject this status
+	// update and wait for the next unless the play queue has recently
+	// been emptied or the connection to MPD has been lost ...
+	if (status->songId() != pimpl->currentSong.id && status->songId() != -1) {
+		return;
+	}
 
-    if (status!=pimpl->status) {
-        pimpl->status = status;
-    }
+	if (status != pimpl->status) {
+		pimpl->status = status;
+	}
 
-    MPNowPlayingInfoCenter *infoCenter = [MPNowPlayingInfoCenter defaultCenter];
+	MPNowPlayingInfoCenter* infoCenter = [MPNowPlayingInfoCenter defaultCenter];
 
-    switch (status->state()) {
-        case MPDState_Playing: {
-            infoCenter.playbackState = MPNowPlayingPlaybackStatePlaying;
-            [pimpl->nowPlayingInfo setObject:@(1) forKey:MPNowPlayingInfoPropertyPlaybackRate];
-            break;
-        }
-        case MPDState_Paused: {
-            infoCenter.playbackState = MPNowPlayingPlaybackStatePaused;
-            [pimpl->nowPlayingInfo setObject:@(0) forKey:MPNowPlayingInfoPropertyPlaybackRate];
-            break;
-        }
-        default:
-        case MPDState_Stopped: {
-            infoCenter.playbackState = MPNowPlayingPlaybackStateStopped;
-            [pimpl->nowPlayingInfo setObject:@(0) forKey:MPNowPlayingInfoPropertyPlaybackRate];
-            break;
-        }
-    }
+	switch (status->state()) {
+	case MPDState_Playing: {
+		infoCenter.playbackState = MPNowPlayingPlaybackStatePlaying;
+		[pimpl->nowPlayingInfo setObject:@(1) forKey:MPNowPlayingInfoPropertyPlaybackRate];
+		break;
+	}
+	case MPDState_Paused: {
+		infoCenter.playbackState = MPNowPlayingPlaybackStatePaused;
+		[pimpl->nowPlayingInfo setObject:@(0) forKey:MPNowPlayingInfoPropertyPlaybackRate];
+		break;
+	}
+	default:
+	case MPDState_Stopped: {
+		infoCenter.playbackState = MPNowPlayingPlaybackStateStopped;
+		[pimpl->nowPlayingInfo setObject:@(0) forKey:MPNowPlayingInfoPropertyPlaybackRate];
+		break;
+	}
+	}
 
-    [pimpl->nowPlayingInfo setObject:@(status->guessedElapsed()) forKey:MPNowPlayingInfoPropertyElapsedPlaybackTime];
-    [pimpl->nowPlayingInfo setObject:@(status->timeTotal()) forKey:MPMediaItemPropertyPlaybackDuration];
-    infoCenter.nowPlayingInfo = pimpl->nowPlayingInfo;
+	[pimpl->nowPlayingInfo setObject:@(status->guessedElapsed()) forKey:MPNowPlayingInfoPropertyElapsedPlaybackTime];
+	[pimpl->nowPlayingInfo setObject:@(status->timeTotal()) forKey:MPMediaItemPropertyPlaybackDuration];
+	infoCenter.nowPlayingInfo = pimpl->nowPlayingInfo;
 }
 
-void MacNowPlaying::updateCurrentCover(const QString &fileName)
+void MacNowPlaying::updateCurrentCover(const QString& fileName)
 {
-    if (fileName!=pimpl->currentCover) {
-        pimpl->currentCover=fileName;
-        NSImage *image = [[NSImage alloc] initByReferencingFile:pimpl->currentCover.toNSString()];
-        MPMediaItemArtwork *artwork = [[MPMediaItemArtwork alloc] initWithBoundsSize:CGSizeMake(1000, 1000) requestHandler:^NSImage * _Nonnull(CGSize) {
-            return image;
-        }];
-        [pimpl->nowPlayingInfo setObject:artwork forKey:MPMediaItemPropertyArtwork];
-        [MPNowPlayingInfoCenter defaultCenter].nowPlayingInfo = pimpl->nowPlayingInfo;
-    }
+	if (fileName != pimpl->currentCover) {
+		pimpl->currentCover = fileName;
+		NSImage* image = [[NSImage alloc] initByReferencingFile:pimpl->currentCover.toNSString()];
+		MPMediaItemArtwork* artwork = [[MPMediaItemArtwork alloc] initWithBoundsSize:CGSizeMake(1000, 1000)
+																	  requestHandler:^NSImage* _Nonnull(CGSize) {
+																		return image;
+																	  }];
+		[pimpl->nowPlayingInfo setObject:artwork forKey:MPMediaItemPropertyArtwork];
+		[MPNowPlayingInfoCenter defaultCenter].nowPlayingInfo = pimpl->nowPlayingInfo;
+	}
 }
 
-void MacNowPlaying::updateCurrentSong(const Song &song)
+void MacNowPlaying::updateCurrentSong(const Song& song)
 {
-    pimpl->currentSong = song;
+	pimpl->currentSong = song;
 
-    [pimpl->nowPlayingInfo setObject:@(song.id) forKey:MPMediaItemPropertyPersistentID];
-    [pimpl->nowPlayingInfo setObject:@(MPMediaTypeAnyAudio) forKey:MPMediaItemPropertyMediaType];
-    [pimpl->nowPlayingInfo setObject:song.title.toNSString() forKey:MPMediaItemPropertyTitle];
-    [pimpl->nowPlayingInfo setObject:song.artist.toNSString() forKey:MPMediaItemPropertyArtist];
-    [pimpl->nowPlayingInfo setObject:song.album.toNSString() forKey:MPMediaItemPropertyAlbumTitle];
+	[pimpl->nowPlayingInfo setObject:@(song.id) forKey:MPMediaItemPropertyPersistentID];
+	[pimpl->nowPlayingInfo setObject:@(MPMediaTypeAnyAudio) forKey:MPMediaItemPropertyMediaType];
+	[pimpl->nowPlayingInfo setObject:song.title.toNSString() forKey:MPMediaItemPropertyTitle];
+	[pimpl->nowPlayingInfo setObject:song.artist.toNSString() forKey:MPMediaItemPropertyArtist];
+	[pimpl->nowPlayingInfo setObject:song.album.toNSString() forKey:MPMediaItemPropertyAlbumTitle];
 
-    [MPNowPlayingInfoCenter defaultCenter].nowPlayingInfo = pimpl->nowPlayingInfo;
+	[MPNowPlayingInfoCenter defaultCenter].nowPlayingInfo = pimpl->nowPlayingInfo;
 }
 
 #include "moc_macnowplaying.cpp"
